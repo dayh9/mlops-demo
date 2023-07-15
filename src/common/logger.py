dbf0b72ab788b300014032d69a14e125440c3822
dayh9 <hrycalikd@gmail.com>
2023-07-15 14:58:40 +0200

    Format logger

--- src/common/logger.py
+++ src/common/logger.py
@@ -5,7 +5,9 @@ import os
 def get_logger(name):
     logger = logging.getLogger(name)
     handler = logging.StreamHandler()
-    handler.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
+    handler.setFormatter(
+        logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    )
     level = os.getenv("LOG_LEVEL", logging.INFO)
     logger.setLevel(int(level))
     logger.addHandler(handler)
